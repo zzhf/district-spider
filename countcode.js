a30d31faa6e14fd0564d91281efedf8e828b23ad
zzhf <zzzhf00@sina.com>
2017-10-14 23:51:06 +0800

    新增乡镇级别数据

--- countcode.js
+++ countcode.js
@@ -11,19 +11,21 @@ const Ajax = (()=>{
     let endCount = 0;
 
     let _get = (url, callback) => {
-        request({url: url, encoding: null}, (err, res, body) => {
-            endCount ++;
-            console.log('请求' + url + '---' + (new Date()).toLocaleTimeString());
-            if (err) {
-                console.error(err);
-            }
-            if (!err && res.statusCode == 200) {
-                let $ =  cheerio.load(Iconv.decode(body, 'gb2312').toString());
-                callback($);
-            } else {
-                // console.error(res);
-            }
-        })
+        setTimeout(() => {
+            request({url: url, encoding: null}, (err, res, body) => {
+                endCount ++;
+                console.log('请求' + url + '---' + (new Date()).toLocaleTimeString());
+                if (err) {
+                    console.error(err);
+                }
+                if (!err && res.statusCode == 200) {
+                    let $ =  cheerio.load(Iconv.decode(body, 'gb2312').toString());
+                    callback($);
+                } else {
+                    // console.error(res);
+                }
+            })
+        }, 500)
     };
 
     (() => {
@@ -59,6 +61,7 @@ const spider = {
     },
 
     getProviceData($, lastUrl) {
+        let that = this;
         let $provice = $('.provincetr>td>a');
         $provice.each((i, e) => {
             let $e = $(e);
@@ -79,57 +82,116 @@ const spider = {
                         }) + '\n');
                     }
                     Ajax.get(cityUrl, ($) => {
-                        $('.countytr').each((i, e) => {
-                            let $e = $(e);
-                            let code = $e.find('td:first-child a').text();
-                            let name = $e.find('td:last-child a').text();
-                            let countyUrl = cityUrl.substring(0, cityUrl.lastIndexOf('\/') + 1) + $e.find('td:first-child a').attr('href');
-                            if (!code && !name) {
-                                code = $e.find('td:first-child').text();
-                                name = $e.find('td:last-child').text();
-                                countyUrl = null;
-                            }
-                            writeStream.write(JSON.stringify({
-                                name: name,
-                                statcode: code
-                            }) + '\n');
-                            if (countyUrl) {
-                                Ajax.get(countyUrl, ($) => {
-                                    $('.towntr').each((i, e) => {
-                                        let $e = $(e);
-                                        let code = $e.find('td:first-child a').text();
-                                        let name = $e.find('td:last-child a').text();
-                                        let townUrl = countyUrl.substring(0, countyUrl.lastIndexOf('\/') + 1) + $e.find('td:first-child a').attr('href');
-                                        writeStream.write(JSON.stringify({
-                                            name: name,
-                                            statcode: code
-                                        }) + '\n');
-                                        Ajax.get(townUrl, ($) => {
-                                            $('.villagetr').each((i, e) => {
-                                                let $e = $(e);
-                                                let statCode = $($e.find('td')[0]).text();
-                                                let countryCode = $($e.find('td')[1]).text();
-                                                let name = $($e.find('td')[2]).text();
-                                                writeStream.write(JSON.stringify({
-                                                    statCode: statCode,
-                                                    name: name,
-                                                    countryCode: countryCode
-                                                }) + '\n');
-                                            })
-                                        })
-                                    })
-                                })
-                            }
-                        })
+                        that.recursionCounty.getCounty($('.countytr'), null, cityUrl, writeStream);
                     })
                 })
-                // this.proviceItem ++;
-                // this.getProviceData($, lastUrl);
             })
         })
-        
     },
 
+    recursionCounty: (function() {
+        let count = 0;
+        let _$county;
+        let _callback;
+        let that = this;
+        let _cityUrl;
+        let _writeStream;
+
+        return {
+            getCounty($county, callback, cityUrl, writeStream) {
+                console.log('县级计数器' + count);
+                _$county = $county || _$county;
+                _callback = callback || _callback;
+                _cityUrl = cityUrl || _cityUrl;
+                _writeStream = writeStream || _writeStream;
+
+                let $e = _$county.eq(count);
+                let code = $e.find('td:first-child a').text();
+                let name = $e.find('td:last-child a').text();
+                let countyUrl = _cityUrl.substring(0, _cityUrl.lastIndexOf('\/') + 1) + $e.find('td:first-child a').attr('href');
+                if (!code && !name) {
+                    code = $e.find('td:first-child').text();
+                    name = $e.find('td:last-child').text();
+                    countyUrl = null;
+                    count ++;
+                    spider.recursionCounty.getCounty();
+                }
+                _writeStream.write(JSON.stringify({
+                    name: name,
+                    statcode: code
+                }) + '\n');
+                if (countyUrl) {
+                    Ajax.get(countyUrl, ($) => {
+                        spider.recursionTown($('.towntr'), () => {
+                            count ++;
+                            spider.recursionCounty.getCounty();
+                        }, countyUrl, _writeStream);
+                    })
+                }
+            }
+        }
+    })(),
+
+    recursionCity: (function() {
+        let count = 0;
+
+        return {
+            getCity($city, callback) {
+                console.log('城市计数器' + count)
+                if (count >= $city.length) {
+                    count = 0;
+                    callback();
+                    return;
+                }
+                let $e = $($city[count]);
+            }
+        }
+    })(),
+
+    recursionTown: (function() {
+        let count = 0;
+        let _callback;
+        let _countryUrl;
+        let that = this;
+
+        return {
+            getTown($town, callback, countryUrl, writeStream) {
+                console.log('乡镇计数器' + count);
+                _callback = callback || _callback;
+                _countryUrl = countyUrl || _countryUrl;
+                _writeStream = writeStream || _writeStream;
+
+                if (count >= $town.length) {
+                    count = 0;
+                    _callback();
+                    return;
+                }
+                let $e = $town.eq(count);
+                let code = $e.find('td:first-child a').text();
+                let name = $e.find('td:last-child a').text();
+                let townUrl = _countryUrl.substring(0, _countryUrl.lastIndexOf('\/') + 1) + $e.find('td:first-child a').attr('href');
+                _writeStream.write(JSON.stringify({
+                    name: name,
+                    statcode: code
+                }) + '\n');
+                Ajax.get(townUrl, ($) => {
+                    $('.villagetr').each((i, e) => {
+                        let $e = $(e);
+                        let statCode = $($e.find('td')[0]).text();
+                        let countryCode = $($e.find('td')[1]).text();
+                        let name = $($e.find('td')[2]).text();
+                        _writeStream.write(JSON.stringify({
+                            statCode: statCode,
+                            name: name,
+                            countryCode: countryCode
+                        }) + '\n');
+                    })
+                    spider.recursionTown.getTown($town);
+                })
+                count ++;
+            }
+        }
+    })(),
 
     spider(url, selector ,writeStream, callback) {
         request({
